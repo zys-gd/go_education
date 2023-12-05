14e2043067f91d912b6afea9a5b15359ee0f06b8
Yurii Zakharenko <yzakharenko@newage.io>
2023-12-05 17:48:12 +0100

    update farmgame

--- cmd/farmgame/farmgame.go
+++ cmd/farmgame/farmgame.go
@@ -5,12 +5,22 @@ import (
 	"math/rand"
 )
 
-var move interface {
+type move interface {
 	move(m []string) string
 }
 
-var feed interface {
+type feed interface {
 	feed() string
+}
+
+type sleep interface {
+	feed() string
+}
+
+type animalLifecycle interface {
+	move
+	feed
+	sleep
 }
 
 const (
@@ -29,16 +39,27 @@ func (a animal) feed() string {
 	return "eating"
 }
 
+func (a animal) sleep() string {
+	return fmt.Sprintf("The animal '%v' is sleeping now", a.name)
+}
+
 func (a animal) move(m []string) string {
-	randomIndex := rand.Intn(len(m))
-	return fmt.Sprintf("The animal '%v' is doing now: %v", a.name, m[randomIndex])
+	return fmt.Sprintf("The animal '%v' is doing now: %v", a.name, m[rand.Intn(len(m))])
+}
+
+func animalActivity(a animal, isAwake bool, m []string) string {
+	if isAwake {
+		if rand.Intn(2) == 0 {
+			return a.move(m)
+		} else {
+			return a.feed()
+		}
+	}
+
+	return a.sleep()
 }
 
 func main() {
-	// Имплементируйте цикл день/ночь, симулятор должен запускаться для трех 24-часовых марсианских дней (72 часа).
-	// Все животные должны спать от заката до рассвета.
-	// Для каждого часа дня выберите случайное животное, что будет осуществлять какое-то случайное действие — передвигаться или есть.
-	// Для каждого действия должно выводиться описание того, что именно сделал зверь.
 	a := animal{"dog", activeTime(diurnal)}
 
 	movementList := []string{
@@ -48,6 +69,22 @@ func main() {
 		"play",
 		"looking for a food",
 	}
+	sunrize := 7
+	dawn := 19
+	hoursInDay := 24
+	days := 3
+	for i, c := 0, 0; i <= hoursInDay; i++ {
+		if i == hoursInDay {
+			i = 0
+			c++
+		}
+		if c == days {
+			break
+		}
+		isDay := i >= sunrize && i <= dawn
+		isAwake := (a.activeTime == diurnal && isDay == true) || (a.activeTime == nocturnal && isDay == false)
 
-	fmt.Println(a.move(movementList))
+		fmt.Printf("Day: %d; Time of a day: %dh; ", c+1, i)
+		fmt.Println(animalActivity(a, isAwake, movementList))
+	}
 }
